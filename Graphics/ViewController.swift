40d626da933dda72f0e474a89e325a5189c9d58c
Andy Stef <astefanchuk@perpet.io>
2016-09-07 14:32:29 +0300

    Initial Commit

---
+++ Graphics/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Graphics
+//
+//  Created by Andy Stef on 9/7/16.
+//  Copyright © 2016 Andy Stef. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
